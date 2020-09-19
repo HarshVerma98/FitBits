468e594ae9ca49569086d6ece3f1ff934d5800db
Hars Verma <harshverm179@gmail.com>
2020-09-19 11:07:01 +0530

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Harsh on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
