928a463c2a1ab3f3257de1aee6ab3e3cc76ca1a6
Harsh  Verma <bl00d_9@icloud.com>
2020-10-21 20:52:49 +0530

    Setup Pods and Installed Firebase!

--- Fitness/FitnessApp.swift
+++ Fitness/FitnessApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FitnessApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             LandingPage()
         }
+    }
+}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Triggering")
+        FirebaseApp.configure()
+        return true
     }
 }
